e7c8e2009bd37879e8bc6c1f6232a75498932aa6
huashen <2494946808@qq.com>
2025-02-25 21:35:42 +0800

    2.25 tree02

--- treeOrder.go
+++ treeOrder.go
@@ -1,6 +1,9 @@
 package main
 
-import "container/list"
+import (
+	"container/list"
+	"math"
+)
 
 func preorderTraversal1(root *TreeNode) []int {
 	if root == nil {
@@ -136,4 +139,186 @@ func rightSideView(root *TreeNode) []int {
 		}
 	}
 	return res
+}
+
+func averageOfLevels(root *TreeNode) []float64 {
+	if root == nil {
+		return nil
+	}
+	res := make([]float64, 0)
+	queue := list.New()
+	queue.PushBack(root)
+	var sum int
+	for queue.Len() > 0 {
+		levelSize := queue.Len()
+		for i := 0; i < levelSize; i++ {
+			node := queue.Remove(queue.Front()).(*TreeNode)
+			if node.Left != nil {
+				queue.PushBack(node.Left)
+			}
+			if node.Right != nil {
+				queue.PushBack(node.Right)
+			}
+			sum += node.Val
+		}
+		res = append(res, float64(sum)/float64(levelSize))
+		sum = 0
+	}
+	return res
+}
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Children []*Node
+ * }
+ */
+
+type Node struct {
+	Val      int
+	Children []*Node
+}
+
+func levelOrder1(root *Node) [][]int {
+	if root == nil {
+		return nil
+	}
+	res := [][]int{}
+	queue := list.New()
+	queue.PushBack(root)
+	for queue.Len() > 0 {
+		levelSize := queue.Len()
+		var tmp []int
+		for i := 0; i < levelSize; i++ {
+			node := queue.Remove(queue.Front()).(*Node)
+			tmp = append(tmp, node.Val)
+			for _, child := range node.Children {
+				queue.PushBack(child)
+			}
+		}
+		res = append(res, tmp)
+	}
+	return res
+}
+
+func largestValues(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+	queue := list.New()
+	queue.PushBack(root)
+	ans := []int{}
+	for queue.Len() > 0 {
+		temp := math.MinInt64
+		levelSize := queue.Len()
+		for i := 0; i < levelSize; i++ {
+			node := queue.Remove(queue.Front()).(*TreeNode)
+			temp = max(temp, node.Val)
+			if node.Left != nil {
+				queue.PushBack(node.Left)
+			}
+			if node.Right != nil {
+				queue.PushBack(node.Right)
+			}
+		}
+		ans = append(ans, temp)
+	}
+	return ans
+}
+func maxDepth(root *TreeNode) int {
+	ans := 0
+	if root == nil {
+		return 0
+	}
+	queue := list.New()
+	queue.PushBack(root)
+	for queue.Len() > 0 {
+		length := queue.Len()
+		for i := 0; i < length; i++ {
+			node := queue.Remove(queue.Front()).(*TreeNode)
+			if node.Left != nil {
+				queue.PushBack(node.Left)
+			}
+			if node.Right != nil {
+				queue.PushBack(node.Right)
+			}
+		}
+		ans++
+	}
+	return ans
+}
+
+func minDepth(root *TreeNode) int {
+	ans := 0
+	if root == nil {
+		return 0
+	}
+	queue := list.New()
+	queue.PushBack(root)
+	for queue.Len() > 0 {
+		length := queue.Len()
+		for i := 0; i < length; i++ {
+			node := queue.Remove(queue.Front()).(*TreeNode)
+			if node.Left == nil && node.Right == nil { //当前节点没有左右节点，则代表此层是最小层
+				return ans + 1
+			}
+			if node.Left != nil {
+				queue.PushBack(node.Left)
+			}
+			if node.Right != nil {
+				queue.PushBack(node.Right)
+			}
+		}
+		ans++ //记录层数
+	}
+
+	return ans
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left, root.Right = root.Right, root.Left
+	invertTree(root.Left)
+	invertTree(root.Right)
+	return root
+}
+
+func isSymmetric(root *TreeNode) bool {
+	return isMirror(root.Left, root.Right)
+}
+func isMirror(left, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+	if left == nil || right == nil {
+		return false
+	}
+	if left.Val != right.Val {
+		return false
+	}
+	return isMirror(left.Left, right.Right) && isMirror(left.Right, right.Left)
+}
+
+func countNodes(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	leftH, rightH := 0, 0
+	leftNode := root.Left
+	rightNode := root.Right
+	for leftNode != nil {
+		leftNode = leftNode.Left
+		leftH++
+	}
+	for rightNode != nil {
+		rightNode = rightNode.Right
+		rightH++
+	}
+	if leftH == rightH {
+		return (2 << leftH) - 1
+	}
+	return countNodes(root.Left) + countNodes(root.Right) + 1
 }
